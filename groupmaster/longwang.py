fa7f6663769b0c8a1b86694860ab3aafac711377
Watanabe-Asa <65025615+Watanabe-Asa@users.noreply.github.com>
2020-11-13 23:47:31 +0800

    优化图片路径，现在不用改图片名字了

--- groupmaster/longwang.py
+++ groupmaster/longwang.py
@@ -2,19 +2,26 @@ import json
 import re
 import random
 import os
-
-from hoshino import Service,aiorequests,R
+from os import path
+from nonebot import MessageSegment
+import random
+import hoshino
+from hoshino import Service,aiorequests
 from hoshino.util import FreqLimiter, DailyNumberLimiter
 
 _max = 3
 EXCEED_NOTICE = f'您今天已经迫害过{_max}次龙王了，请明早5点后再来！'
 _nlmt = DailyNumberLimiter(_max)
 _flmt = FreqLimiter(5)
+res_path = path.abspath(path.join(path.expanduser(hoshino.config.RES_DIR),'longwang'))
+if not path.exists(res_path):
+	os.makedirs(res_path)
 
 sv = Service('longwang')
 
 @sv.on_command('迫害龙王')
 async def longwang(session):
+    global res_path
     uid = session.ctx['user_id']
     if not _nlmt.check(uid):
         await session.send(EXCEED_NOTICE, at_sender=True)
@@ -27,6 +34,9 @@ async def longwang(session):
     gid = session.ctx['group_id']
     dragon_king=await session.bot.get_group_honor_info(group_id=gid,type='talkative')
     dragon_king=dragon_king['current_talkative']['user_id']
-    longwang = R.img(f"longwang/dragon{random.randint(1, 7)}.jpg").cqcode
+    res_list = os.listdir(res_path)
+    pic = random.choice(res_list)
+    pic_path = path.join(res_path, pic)
     reply=random.choice(['龙王出来挨透','龙王出来喷水'])
-    session.finish(f'[CQ:at,qq={dragon_king}]\n{reply}\n{longwang}')
+    longwang = MessageSegment.image(f'file:///{pic_path}')
+    await session.finish(f'[CQ:at,qq={dragon_king}]\n{reply}\n{longwang}')
