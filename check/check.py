d24a9d50f0e5b0d77469bfb3c697e5f61ee79a0b
しゃけてり <65025615+Watanabe-Asa@users.noreply.github.com>
2020-12-16 02:05:10 +0800

    重构和优化

--- check/check.py
+++ check/check.py
@@ -1,33 +1,22 @@
-from random import choice
 import hoshino
 import nonebot
 from nonebot import scheduler
 from nonebot import on_command, CommandSession
-from hoshino import log, priv
+from hoshino import log, priv, Service
 from .data_source import Check
 
 MAX_PERFORMANCE_PERCENT = hoshino.config.check.MAX_PERFORMANCE_PERCENT
 PROCESS_NAME_LIST = hoshino.config.check.PROCESS_NAME_LIST
-SUPERUSERS = hoshino.config.SUPERUSERS[0]
 
 logger = log.new_logger('check')
 check = Check(hoshino.config.check.PROCESS_NAME_LIST)
 
+sv = Service('check', use_priv=priv.SUPERUSER, manage_priv=priv.SUPERUSER, visible=False) 
 
-@on_command('check', aliases=('自检', '自檢', '自我检查', '自我檢查'), only_to_me=True)
+@sv.on_command('check', aliases=('自检', '自檢', '自我检查', '自我檢查'))
 async def music_recommend(session: CommandSession):
     if not priv.check_priv(session.event, priv.SUPERUSER):
-        check_report = await check.get_check_easy(MAX_PERFORMANCE_PERCENT)
-        more_info = '[CQ:at,qq={}]'.format(choice(list(SUPERUSERS))) if (SUPERUSERS and session.event['message_type'] == 'group') else "\n……请使用反馈功能联系维护哦~"
-        if check_report:
-            await session.send(check_report + more_info)
-            check_report_admin = await check.get_check_info()
-            if check_report_admin and SUPERUSERS:
-                for admin in SUPERUSERS:
-                    await session.send_private_msg(user_id=admin, message='調子がよくないみたいね……')
-                    await session.send_private_msg(user_id=admin, message=check_report_admin)
-        else: 
-            await session.send("大丈夫だよ。心配してくれてありがとう。(人''▽｀)")
+        await session.send("大丈夫だよ。心配してくれてありがとう。")
     else:
         check_report_admin = await check.get_check_info()
         if check_report_admin:
@@ -36,8 +25,10 @@ async def music_recommend(session: CommandSession):
             logger.error("Not found Check Report")
             await session.send("[ERROR]Not found Check Report")
 
+
 @scheduler.scheduled_job('cron', hour='*', minute='13') 
 async def check_task():
     bot = nonebot.get_bot()
-    result = await check.get_check_info()        
-    await bot.send_private_msg(user_id=SUPERUSERS, message=result)
+    weihu = hoshino.config.SUPERUSERS[0]
+    result = await check.get_check_easy()        
+    await bot.send_private_msg(user_id=weihu, message=result)
--- check/data_source.py
+++ check/data_source.py
@@ -11,6 +11,12 @@ class Check():
         self.cpu_percent = 0
         self.memory_percent = 0
         self.disk_percent = 0
+        self.boot_time = 0
+        self.send = 0
+        self.recv = 0
+        self.packets_sent = 0
+        self.packets_recv = 0
+        self.packet_lost = 0
         self.baidu = 404
         self.google = 404
         self.process_name_list = []
@@ -27,43 +33,38 @@ class Check():
     async def get_check_info(self):
         self.run_all_check()
         putline = []
-
-        putline.append("--Performance--\n[Cpu] {}%\n[Memory] {}%\n[Disk] {}%\n--Network--\n[Baidu] {}\n[Google] {}".format(self.cpu_percent, self.memory_percent, self.disk_percent, self.baidu, self.google))
-        
+        putline.append("--Performance--\n[Cpu] {}%\n[Memory] {}%\n[Disk] {}%\n[Boot time] {}\n--Network--\n[Send] {}MB\n[Receive] {}MB\n[Packets sent] {}\n[Packets recv] {}\n[Packets lost] {}%".format(self.cpu_percent, self.memory_percent, self.disk_percent, self.boot_time, self.send, self.recv, self.packets_sent, self.packets_recv, self.packet_lost))        
         if self.process_name_list:
             putline.append("--Process--")
             for name, status in zip(self.process_name_list, self.process_status_list):
-                putline.append("[{}] {}".format(name, status))
-        
+                putline.append("[{}] {}".format(name, status))      
         if self.user_list:
             putline.append("--Users--")
             for user in self.user_list:
-                putline.append("[{}] {}".format(user['name'], user['started']))
-        
+                putline.append("[{}] {}".format(user['name'], user['started']))       
         return "\n".join(putline)
 
     async def get_check_easy(self, max_performance_percent=[92,92,92]):
+        self.run_all_check()
         putline = []
         check_list = await self.get_check_simple()
-
         if sum(check_list) != 0:
             logger.error("Computer problem detected. check code: {}".format(check_list))
             if sum(check_list) == 5:
-                return "我去世了……（安详"
+                return "我去世了……（安详\n⚠请在群聊中发送自检指令获取详细信息"
             if sum(check_list) == 4:
-                return "你们如果还能看到消息一定是奇迹……"
+                return "如果还能看到消息那一定是奇迹……\n⚠请在群聊中发送自检指令获取详细信息"
             if sum(check_list[:3]) != 0:
-                return "啊……我感觉……好热……"
-            if sum(check_list[3:5]) == 2:
-                return "谁拔我网线?!"
+                return "啊……我感觉……好热……\n⚠请在群聊中发送自检指令获取详细信息"
             if check_list[4] == 1:
-                return "我节点又断惹(部分功能会受到影响)"
-            return "我似乎出了点问题……"
+                return "我节点断了……(部分功能会受到影响)"
+        else:
+            putline.append("当前网络状态：\n上传：{}MB\n接收：{}MB\n丢包率: {}%\n※请留意服务器的运行状态".format(self.send, self.recv, self.packet_lost))
+            return "\n".join(putline)
 
     async def get_check_simple(self, max_performance_percent=[92,92,92]) -> list:
         check_list = [0,0,0,0,0,0]
         self.run_all_check()
-
         if self.cpu_percent > max_performance_percent[0]:
             check_list[0] = 1
         if self.memory_percent > max_performance_percent[1]:
@@ -77,16 +78,24 @@ class Check():
         for status in self.process_status_list:
             if status != 'running':
                 check_list[5] = 1
-                break
-        
+                break      
         return check_list
 
     def get_performance_check(self):
         self.cpu_percent = psutil.cpu_percent()
         self.memory_percent = psutil.virtual_memory().percent
         self.disk_percent = psutil.disk_usage("/").percent
+        self.boot_time = datetime.fromtimestamp(
+        psutil.boot_time()).strftime("%Y-%m-%d %H: %M: %S")
 
     def get_network_check(self):
+        self.send = round(psutil.net_io_counters().bytes_sent/1024/1024, 2)
+        self.recv = round(psutil.net_io_counters().bytes_recv/1024/1024, 2)
+        self.packets_sent = psutil.net_io_counters().packets_sent
+        self.packets_recv = psutil.net_io_counters().packets_recv
+        self.packet_lost = round(
+        psutil.net_io_counters().packets_recv / psutil.net_io_counters().packets_sent, 2
+    )
         try:
             self.baidu = requests.get("http://www.baidu.com").status_code
         except:
